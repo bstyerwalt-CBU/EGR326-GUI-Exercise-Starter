20a82bd21a1df28baa09bf5242d9e54fca335a79
bstyerwalt@calbaptist.edu <bstyerwalt@calbaptist.edu>
2022-03-28 23:40:30 -0700

    reverting to starter conditions

---
+++ src/TextEditorGUI.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintStream;
+
+
+public class TextEditorGUI {
+    private JButton saveButton;
+    private JButton button2;
+    private JButton southwest;
+    private JButton southeast;
+    private JFrame frame;
+    private JTextArea textArea;
+
+    public TextEditorGUI() {
+        createComponents();
+        createFrame();
+        createContainer();
+    }
+
+    private void createContainer() {
+        // your code here
+    }
+
+    private void createFrame() {
+        // your code here
+    }
+
+    private void createComponents() {
+        // your code here
+    }
+
+
+    // Clicks "Save As"
+    // Implement "Save As" Action Listener as a private nested class
+
+
+}
